a2c541b7864a708e9cd74709c4f32733fadb326b
Maria Bezzubova <anmari1112@gmail.com>
2025-06-04 11:50:18 +0300

    component without JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import React from "react";
+
 const now = new Date();
 
 export const App = () => {
-	return (
-		<div>
-			<main>
-				<h1>Hello</h1>
-			</main>
-			<footer>{now.getFullYear()}</footer>
-		</div>
+	return React.createElement(
+		"div",
+		null,
+		React.createElement(
+			"main",
+			null,
+			React.createElement("h1", null, "Hello"),
+		),
+		React.createElement("footer", null, now.getFullYear()),
 	);
 };
